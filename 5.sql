f17ee09e3a7a85d13670e9e0747ddfa2a04c1f27
Aarif Mahdi <mahdiaarif6@gmail.com>
2026-05-14 21:04:32 +0500

    Add SQL query for patient admissions filtering
    Added SQL query to select patient details based on specific criteria.

--- 5.sql
+++ 5.sql
@@ -1,3 +1,10 @@
+/*
+Show patient_id, attending_doctor_id, and diagnosis for admissions that match one of the two criteria:
+1. patient_id is an odd number and attending_doctor_id is either 1, 5, or 19.
+2. attending_doctor_id contains a 2 and the length of patient_id is 3 characters.
+*/
+
+
 SELECT 
 	patient_id,
     attending_doctor_id,
